aecf90a018d9bc67301ca922832f18aefbe1eef5
surya <bogadhi.surya424@gmail.com>
2017-12-04 11:27:21 +0530

    Eshopper design integration

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes} from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductinfoComponent } from './productinfo/productinfo.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ContactComponent } from './contact/contact.component';
+import { AccountComponent } from './account/account.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
+const myrouteurls : Routes = [
+  {path:'',component:BodyComponent},
+ 
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    BodyComponent,
+    FooterComponent,
+    LoginComponent,
+    ProductsComponent,
+    ProductinfoComponent,
+    CartComponent,
+    CheckoutComponent,
+    WishlistComponent,
+    ContactComponent,
+    AccountComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(myrouteurls)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
